c8c3bd76194b2c310527100569a7afcd0945d631
hojunlee-hj <cajun7@naver.com>
2021-05-03 18:19:13 +0900

    React-Native-Express-Mysql

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+var mysql = require('mysql');
+const cors = require('cors');
+
+var http = require('http');
+var app = express();
+app.use(cors());
+// bodyParser (in express)
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+var server = http.createServer(app);
+
+const db = mysql.createConnection({
+    host: "localhost",
+    port: '3306',
+    user: "root",
+    password: "0000",
+    database: "refrigerator",
+});
+
+db.connect((err)=>{
+    if(err){
+        throw err;
+    }
+    console.log("Mysql connected!!");
+});
+
+app.get("/getUser",(req,res)=>{
+    db.query('SELECT * FROM user', (err,rows)=>{
+        if(err) console.log(err);
+        else {
+            console.log(res);
+            console.log(rows);
+            res.json(rows);}
+    });
+});
+
+app.get('/', function(req,res){
+    res.send('root page');
+})
+
+app.get('/start', function(req,res){
+    res.send('start page');
+})
+
+
+server.listen(3344, ()=>{
+    console.log('Server listen on port' + server.address().port);
+})
---
+++ dbtest.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+
+class Test extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {clicked:false, data: []};
+    }
+click = async ()  => {
+    this.setState(({clicked: true}));
+ await fetch('http://172.30.1.21:3344/getUser',{
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    },
+}).then(response=>response.json()).then((response) => this.setState({data:response}));
+console.log(this.state.data);
+};
+
+renderCategories(){
+    return this.state.data.map((data)=> <View><Text>{data.usr_Name}</Text>
+    <Text>{data.usr_Email}</Text>
+    <Text>{data.usr_Nickname}</Text></View>)
+}
+    
+
+    unclick =() => {this.setState(({clicked: false})); console.log(this.state.data, this.state.clicked)};
+
+    render(){
+        return(
+            <View >
+                <Text>GET TEXT BUTTON </Text>
+                {
+                    !this.state.clicked ? <Button title="get" onPress={this.click} /> : this.renderCategories()
+                }
+                {
+                    this.state.clicked ? <Button title="cancle" onPress={this.unclick} /> : <Text>PUSH BUTTON!</Text>
+                }
+            </View>
+        )
+    }
+}
+
+export default Test;
